f628f56e56d24e41b31c28298595d934725f0dc1
Roger Lipscombe <roger@differentpla.net>
2018-11-30 10:54:58 +0000

    Initial creation

---
+++ lib/dns_ex.ex
@@ -0,0 +1,18 @@
+defmodule DnsEx do
+  @moduledoc """
+  Documentation for DnsEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DnsEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dns_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule DnsExTest do
+  use ExUnit.Case
+  doctest DnsEx
+
+  test "greets the world" do
+    assert DnsEx.hello() == :world
+  end
+end
